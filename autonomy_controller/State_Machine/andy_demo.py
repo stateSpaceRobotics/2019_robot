bdfebbff68e8cd38e78a5e75ece371796adfa7d7
Viraj <vrp56@msstate.edu>
2019-04-06 16:54:32 -0500

    Andy demo 3b

--- autonomy_controller/State_Machine/andy_demo.py
+++ autonomy_controller/State_Machine/andy_demo.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 #I do not know what I am importing, please send help!
 import roslib
 import rospy
@@ -7,6 +8,7 @@ import tf2_ros
 from std_msgs.msg import Bool
 from move_base_msgs.msg import *
 from geometry_msgs.msg import Twist
+import actionlib
 #Seriously do not know what we are importing
 
 #define state Idle
@@ -17,31 +19,34 @@ class Idle(smach.State):
                              input_keys=['Idle_counter_in', 'e_stop'],
                              output_keys=['Idle_counter_out'])
     
-    def execute(self):
+    def execute(self, userdata):
         #Run the state
+        rospy.loginfo('Idle')
         return 'run'
 
 
 #define state Kill
-class Kill():
+class Kill(smach.State):
     def __init__(self):
         smach.State.__init__(self,
                              outcomes=['kill'])
                             
     #The Kill state ends the program
-    def execute(self,userdata):
+    def execute(self, userdata):
+        rospy.loginfo('Kill')
         return 'kill'
 
 
 #define state Stuck
-class Stuck():
+class Stuck(smach.State):
     def __init__(self):
         smach.State.__init__(self,
                              outcomes=['stuck', 'death'],
-                             in_keys=['e_stop'])
+                             input_keys=['e_stop'])
         
     def execute(self, userdata):
         #Check to see if the user wants to end
+        rospy.loginfo('Stuck')
         if userdata.e_stop == True:
             return 'death'
 
@@ -53,6 +58,7 @@ class Stuck():
 
 def run_andy_main():
     rospy.init_node('smach_example_state_machine')
+    rospy.loginfo('begin')
 
     # Create a SMACH state machine
     sm_andy = smach.StateMachine(outcomes=['end'])
@@ -61,10 +67,16 @@ def run_andy_main():
     sm_andy.userdata.e_stop = False
 
     # Did not include coordinates yet, is blank
-    sm_andy.userdata.move_array[[0.960, -0.280], [0.843, 0.537], [0.960, -0.280]]
-    global move_index
-    move_index = 0
-    
+    # sm_andy.userdata.move_array[[0.960, -0.280], [0.843, 0.537], [0.960, -0.280]]
+    # global move_index
+    # move_index = 0
+    rospy.loginfo('Create Server')
+    andy = actionlib.SimpleActionClient(
+        '/dumper/move_base', MoveBaseAction
+    )
+    rospy.loginfo('Wait')
+    andy.wait_for_server()
+    rospy.loginfo('Server up')
     # Open the container
     with sm_andy:
         #Add states: 
@@ -76,44 +88,46 @@ def run_andy_main():
                                           'e_stop': 'e_stop'})
 
         #initial starting condition
-        def drive_goal_andy():
+        def drive_goal_andy(self, userdata):
+            rospy.loginfo('drive_goal_andy')
             drive_goal = MoveBaseGoal()
             drive_goal.target_pose.header.frame_id = "/map"
             drive_goal.target_pose.header.stamp = rospy.get_rostime()
 
-            drive_goal.target_pose.pose.position.x = 1
-            drive_goal.target_pose.pose.position.y = 1
+            drive_goal.target_pose.pose.position.x = 1.5
+            drive_goal.target_pose.pose.position.y = 1.5
             drive_goal.target_pose.pose.orientation.w = 1
             drive_goal.target_pose.pose.orientation.z = 0
 
             return drive_goal
 
         smach.StateMachine.add('Drive',
-                               smach_ros.SimpleActionState('/dumper/move_base_recovery',
+                               smach_ros.SimpleActionState('/dumper/move_base',
                                                            MoveBaseAction,
                                                            goal_cb = drive_goal_andy,
                                                            input_keys=['']), #N/A
-                               transitions={'motion':'Move1',
-                                            'no_motion':'Stuck',
+                               transitions={'succeeded':'Move1',
+                                            'aborted':'Stuck',
                                             'preempted': 'Drive'},
                                remapping={''}) #N/A
 
         #First of Four Movements
         def move1_goal_andy(self, userdata):
+            rospy.loginfo('move1_goal_andy')
             move_goal = MoveBaseGoal()
             move_goal.target_pose.header.frame_id = "/map"
             move_goal.target_pose.header.stamp = rospy.get_rostime()
 
             #Figure out orientation and position during testing
-            move_goal.target_pose.pose.position.x = 1
-            move_goal.target_pose.pose.position.y = 2
+            move_goal.target_pose.pose.position.x = 2.5
+            move_goal.target_pose.pose.position.y = 1.5
             move_goal.target_pose.pose.orientation.w = 0.707
             move_goal.target_pose.pose.orientation.z = 0.707
 
             return move_goal
 
         smach.StateMachine.add('Move1',
-                               smach_ros.SimpleActionState('/dumper/move_base_recovery',
+                               smach_ros.SimpleActionState('/dumper/move_base',
                                                            MoveBaseAction,
                                                            goal_cb = move1_goal_andy,
                                                            input_keys=['aruco_pose']), #N/A
@@ -125,20 +139,21 @@ def run_andy_main():
 
         #Second of Four Movements
         def move2_goal_andy(self, userdata):
+            rospy.loginfo('move2_goal_andy')
             move_goal = MoveBaseGoal()
             move_goal.target_pose.header.frame_id = "/map"
             move_goal.target_pose.header.stamp = rospy.get_rostime()
 
             #Figure out orientation and position during testing
-            move_goal.target_pose.pose.position.x = 2
-            move_goal.target_pose.pose.position.y = 2
+            move_goal.target_pose.pose.position.x = 2.5
+            move_goal.target_pose.pose.position.y = 2.5
             move_goal.target_pose.pose.orientation.w = 0
             move_goal.target_pose.pose.orientation.z = 1
 
             return move_goal
 
         smach.StateMachine.add('Move2',
-                               smach_ros.SimpleActionState('/dumper/move_base_recovery',
+                               smach_ros.SimpleActionState('/dumper/move_base',
                                                            MoveBaseAction,
                                                            goal_cb = move2_goal_andy,
                                                            input_keys=['aruco_pose']), #N/A
@@ -149,20 +164,21 @@ def run_andy_main():
 
         #Third of Four Movements
         def move3_goal_andy(self, userdata):
+            rospy.loginfo('move3_goal_andy')
             move_goal = MoveBaseGoal()
             move_goal.target_pose.header.frame_id = "/map"
             move_goal.target_pose.header.stamp = rospy.get_rostime()
 
             #Figure out orientation and position during testing
-            move_goal.target_pose.pose.position.x = 2
-            move_goal.target_pose.pose.position.y = 1
+            move_goal.target_pose.pose.position.x = 1.5
+            move_goal.target_pose.pose.position.y = 2.5
             move_goal.target_pose.pose.orientation.w = -0.707
             move_goal.target_pose.pose.orientation.z = 0.707
 
             return move_goal
 
         smach.StateMachine.add('Move3',
-                               smach_ros.SimpleActionState('/dumper/move_base_recovery',
+                               smach_ros.SimpleActionState('/dumper/move_base',
                                                            MoveBaseAction,
                                                            goal_cb = move3_goal_andy,
                                                            input_keys=['aruco_pose']), #N/A
@@ -189,7 +205,7 @@ def run_andy_main():
         #     return move_goal
 
         # smach.StateMachine.add('Move4',
-        #                        smach_ros.SimpleActionState('/dumper/move_base_recovery',
+        #                        smach_ros.SimpleActionState('/dumper/move_base',
         #                                                    MoveBaseAction,
         #                                                    goal_cb = move4_goal_andy,
         #                                                    input_keys=['aruco_pose']), #N/A
@@ -201,14 +217,16 @@ def run_andy_main():
         #-------------------------------------------------------
                                
         smach.StateMachine.add('Kill', Kill(),
-                               transitions={'kill': 'end'})
-                               
+                                transitions={'kill': 'end'})
+                                
         smach.StateMachine.add('Stuck', Stuck(),
-                               transitions={'stuck': 'Idle',
+                                transitions={'stuck': 'Idle',
                                             'death': 'Kill'},
-                               remapping={'e_stop': 'e_stop'})
+                                remapping={'e_stop': 'e_stop'})
 
-        return sm_andy
+    outcome = sm_andy.execute()
+    rospy.spin()
+    outcome.stop()
 
 
 
